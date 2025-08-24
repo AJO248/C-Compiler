337bd977c806eb515b8fff011c93b2f6b8eb4005
Arjun <94214966+AJO248@users.noreply.github.com>
2025-08-24 15:49:59 +0530

    fixed float errors

--- tests/simple.c
+++ tests/simple.c
@@ -2,9 +2,10 @@
 #define MAX 100
 int main(void)
 {
-    int *a = 10;
-    float b = 3.14;
+    int a = 10;
+    float b = 2.3;
     char c = 'x';
     printf("Hello, world! %d %f %c\n", a, b, c);
+
     return 0;
 }
